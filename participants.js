0588e7d327d95634b1dfed6c013b604c6c5c5378
Joy <caronramos@Carons-MacBook-Air.local>
2022-04-22 15:24:10 -0700

    Added participants js and html

---
+++ participants.js
@@ -0,0 +1,82 @@
+const participants = [
+    {
+        id: 3,
+        name: "Jane Doe",
+        level: "Beginner",
+        fee: 25
+    }
+];
+
+$(() => {
+    renderParticipants();
+})
+
+function renderParticipants() {
+    $("#participants-container").empty().append(
+        participants.map(p => renderParticipant(p))
+    );
+}
+
+function renderParticipant(participant) {
+    return (
+        `<tr>
+            <td>${participant.name}</td>
+            <td>${participant.level}</td>
+            <td>$${participant.fee}</td>
+            <td class="text-end">
+                <button class="btn btn-primary btn-sm" onclick="openEditParticipantModal(${participant.id})">Update</button>
+                <button class="btn btn-danger btn-sm" onclick="deleteParticipant(${participant.id})">Delete</button>
+            </td>
+        </tr>`
+    )
+}
+
+const editParticipantModal = new bootstrap.Modal(document.getElementById('edit-participant-modal'))
+let currentEditParticipantId = -1;
+let nextParticipantId = 0;
+
+function openEditParticipantModal(id) {
+    let participant = participants.find(p => p.id === id);
+
+    if(!participant) {
+        participant = {
+            id: nextParticipantId,
+            name: "New Participant",
+            level: "Advanced",
+            fee: 30
+        }
+        nextParticipantId++;
+    }
+
+    currentEditParticipantId = participant.id;
+
+    $("#edit-participant-modal-title").text(`Edit ${participant.name}`)
+    $("#participant-name-input").val(participant.name);
+    $("#participa t-level-input").val(participant.level);
+    $("#participant-fee-input").val(participant.fee);
+
+    editParticipantModal.show();
+}
+
+function saveEditParticipantModal() {
+    let participant = participants.find(p => p.id === currentEditParticipantId);
+
+    if(!participant) {
+        participant = { id: currentEditParticipantId };
+        participants.push(participant);
+    }
+
+    participant.name = $("#participant-name-input").val();
+    participant.level = $("#participant-level-input").val();
+    participant.price = $("#participant-fee-input").val();
+
+    editParticipantModal.hide();
+
+    renderParticipants();
+}
+
+function deleteParticipant(id) {
+    const participantIndex = participants.findIndex(p => p.id === id);
+    participants.splice(participantIndex, 1);
+    renderParticipants();
+}
